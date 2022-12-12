3fe15595a4df8d59175d5204c90b9e045becbc49
Hao YE <hao.ye@epita.fr>
2022-12-12 22:26:20 +0100

    feat: add recall, precision, and confusion matrix

--- train/test_model.py
+++ train/test_model.py
@@ -1,5 +1,6 @@
 import joblib
 import pandas as pd
+import matplotlib.pyplot as plt
 
 def test_model():
     # Load model
@@ -15,6 +16,27 @@ def test_model():
     # Evaluate model
     score = pipe.score(x, y)
     print("Model accuracy: ", score)
+
+    # Recall
+    y_pred = pipe.predict(x)
+    from sklearn.metrics import recall_score
+    recall = recall_score(y, y_pred)
+    print("Recall: ", recall)
+
+    # Precision
+    from sklearn.metrics import precision_score
+    precision = precision_score(y, y_pred)
+    print("Precision: ", precision)
+
+    #Save the confusion matrix
+    from sklearn.metrics import confusion_matrix
+    cm = confusion_matrix(y, y_pred)
+    plt.matshow(cm)
+    plt.title('Confusion matrix')
+    plt.colorbar()
+    plt.ylabel('True label')
+    plt.xlabel('Predicted label')
+    plt.savefig('train/img/confusion_matrix.png')
 
 if __name__ == "__main__":
     test_model()
